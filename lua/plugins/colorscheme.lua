46c8eac20bcd4976965f575eac4c65f0b714cc43
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-03 22:54:00 +0700

    chore: membuat colorscheme menjadi transparan

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,16 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("github-theme").setup({
+			options = {
+				transparent = true,
+				styles = {
+					comments = "italic",
+					functions = "bold",
+					keywords = "italic",
+				},
+			},
+		})
 		vim.cmd("colorscheme github_dark_default")
 	end,
 }
