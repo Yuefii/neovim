399dad2ed9e59bc2c0bcd105237360e1da3cdb90
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-07 02:17:28 +0700

    chore: menambahkan keymaps & refact noted

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,53 +6,94 @@ local opts = {
 	silent = true,
 }
 
--- NOTE: Nonaktifkan default behavior pada spasi dalam mode normal dan visual
+-- NOTE:
+-- Nonaktifkan default behavior pada spasi dalam mode normal dan visual
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- NOTE: Menyimpan file
+-- NOTE:
+-- Menyimpan file
+-- (Ctrl+S)
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
 
--- NOTE: Berpindah text keatas dan kebawah
+-- NOTE:
+-- Berpindah text keatas dan kebawah
+-- (alt+Up) (alt+Down)
 vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
 
--- NOTE: Clear highlights
+-- NOTE:
+-- Clear highlights
+-- (Space)
 vim.keymap.set("n", "<Esc>", ":noh<CR>", opts)
 
--- NOTE: Quit file
+-- NOTE:
+-- Quit file
+-- (Space+q)
 vim.keymap.set("n", "<leader>q", "<cmd> q <CR>", opts)
 
--- NOTE: Membuat jendela vertikal atau horizontal (seperti Ctrl+\ di VS Code)
-vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<CR>", opts)
+-- NOTE:
+-- Membuat jendela vertikal atau horizontal
+-- (Space+sh) horizontal
+-- (Space+sv) vertikal
 vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>", opts)
 vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", opts)
 
--- NOTE: move between splits seperti di VS Code
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+-- NOTE:
+-- Move between split
+-- (Ctrl+Up)
+-- (Ctrl+Down)
+-- (Ctrl+Left)
+-- (Ctrl+Right)
+vim.keymap.set("n", "<C-Up>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-Down>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-Left>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-Right>", "<C-w>l", opts)
 
--- NOTE: Copy (Ctrl+C) di mode visual: menyalin ke clipboard
+-- NOTE:
+-- Select semua text di mode normal
+-- (Ctrl+A)
+vim.api.nvim_set_keymap("n", "<C-A>", "ggVG", opts)
+
+-- NOTE: Copy di mode visual: menyalin ke clipboard
+-- (Ctrl+C)
 vim.keymap.set("v", "<C-c>", '"+y', opts)
 
--- NOTE: Cut (Ctrl+X) di mode visual: memotong ke clipboard
+-- NOTE:
+-- Cut di mode visual: memotong ke clipboard
+-- (Ctrl+X)
 vim.keymap.set("v", "<C-x>", '"+d', opts)
 
--- NOTE: Paste (Ctrl+V) di mode visual: ganti teks terpilih dengan clipboard
+-- NOTE:
+-- Paste di mode visual: ganti teks terpilih dengan clipboard
+-- (Ctrl+V)
 vim.keymap.set("v", "<C-v>", '"+p', opts)
 
--- NOTE: Menutup buffer saat ini
+-- NOTE:
+-- Berpindah buffer
+-- (Space+Tab)
+vim.keymap.set("n", "<leader><Tab>", ":bnext<CR>", opts)
+
+-- NOTE:
+-- Menutup buffer saat ini
+-- (Space+x)
 vim.keymap.set("n", "<leader>x", ":bd<CR>", opts)
 
--- NOTE: Undo (Ctrl+Z) di mode normal
+-- NOTE:
+-- Undo di mode normal
+-- (Ctrl+Z)
 vim.keymap.set("n", "<C-z>", "u", opts)
 
--- NOTE: Redo (Ctrl+Y) di mode normal
+-- NOTE:
+-- Redo di mode normal
+-- (Ctrl+Y)
 vim.keymap.set("n", "<C-y>", "<C-r>", opts)
 
--- NOTE: close insert di terminal
-vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", opts)
+-- NOTE:
+-- Close insert di terminal
+-- (Space+Esc)
+vim.keymap.set("t", "<leader><esc>", "<C-\\><C-n>", opts)
 
--- NOTE: membuat floating terminal
+-- NOTE:
+-- Membuat floating terminal
+-- (Space+t)
 vim.keymap.set("n", "<leader>t", ":Floaterminal<CR>", opts)
